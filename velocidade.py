5ba464d902dffe538e113b168b7b434f479e3fce
AndreVVoigt <114924598+AndreVVoigt@users.noreply.github.com>
2022-10-14 08:02:48 -0300

    Update velocidade.py

--- velocidade.py
+++ velocidade.py
@@ -3,11 +3,13 @@ import random
 largura_tela, altura_tela = pygame.display.get_surface().get_size()
 tela = pygame.display.get_surface()
 
+
 class Velocidade:
     def __init__(self):
-
+        self.duracao_boost = pygame.time.get_ticks() + 1000
         self.tempo_spawn = 0
         self.existe_sprite = False
+        self.boost_ativo = False
     def spawn_velocidade(self):
         if self.existe_sprite:
 
@@ -20,4 +22,12 @@ class Velocidade:
             posicao_velocidade = (random.randint(0, (largura_tela-12)), random.randint(0, (altura_tela-12)))
             pygame.draw.rect(tela, (255, 255, 0), (posicao_velocidade[0], posicao_velocidade[1], 12, 12))
             self.existe_sprite = True
+
+    def duracao_velocidade(self):
+        if pygame.time.get_ticks() < self.duracao_boost:
+            self.boost_ativo = True
+
+        else:
+            self.boost_ativo = False
+            self.duracao_boost = pygame.time.get_ticks()
 
