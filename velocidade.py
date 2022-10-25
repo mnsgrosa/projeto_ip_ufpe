c5149cf4fd5871da42ec1bf8d3c9ead661726033
AndreVVoigt <114924598+AndreVVoigt@users.noreply.github.com>
2022-10-25 10:39:40 -0300

    Update velocidade.py

--- velocidade.py
+++ velocidade.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.math import Vector2
 import random
 largura_tela, altura_tela = pygame.display.get_surface().get_size()
 tela = pygame.display.get_surface()
@@ -14,10 +15,12 @@ class Velocidade:
         self.existe_sprite = False
         self.pos = Vector2(random.randint(0, self.largura_tela - 12), random.randint(0, self.altura_tela - 12))
         self.block = pygame.Rect(self.pos.x, self.pos.y, 12, 12)
+        self.last = pygame.time.get_ticks()
+        self.cooldown = 0
         duracao_boost = False
         boost_ativo = False
 
-    def spawn_vida(self, status):
+    def spawn_velocidade(self, status):
         if self.existe_sprite:
             # Caso já exista um objeto spawnado, o objeto tem seu contador zerado
             self.tempo_spawn = 0
@@ -28,7 +31,6 @@ class Velocidade:
             # Supondo que o jogo rode a 30fps, o contador irá até 600, ou seja, 20 segundos
             # Então o objeto é desenhado aleatoriamente na tela (por enquanto sem sprite)
             # E a variável recebe True até que haja a colisão do jogador com o objeto e variável volte a ser False
-            # Matheus: Nao sei fazer spawnar uma segunda vez tem algum erro na minha logica
             status = False
             self.pos = Vector2(random.randint(0, self.largura_tela-12), random.randint(0, self.altura_tela-12))
             self.block.x = self.pos.x
@@ -50,12 +52,18 @@ class Velocidade:
             self.pos.y = 2000
 
     # So desenha na tela se o sprite existir na tela
-    def draw_vida(self):
+    def draw_velocidade(self):
         if self.existe_sprite:
             pygame.draw.rect(self.tela, (255, 255, 0), self.block)
 
     # funcao que deve se repetir em outros objetos
-    def update_vida(self, status):
-        self.spawn_vida(status)
+    def update_velocidade(self, status):
+        self.spawn_velocidade(status)
         self.coletado(status)
 
+    # funcao que de cooldawn do boost
+    def respawn(self):
+        now = pygame.time.get_ticks()
+        if now - self.last >= self.cooldown:
+            self.last = now
+            self.spawn_velocidade()
