cc82513cb1787791f0dd861df5c42b34ecf77f94
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-10-26 10:44:39 -0300

    Integração com as texturas de vida e velocidade

--- jogador.py
+++ jogador.py
@@ -2,41 +2,46 @@ import pygame
 from pygame.math import Vector2
 
 
-class Jogador:
+# Note que jogador herda da classe Sprite do pygame
+class Jogador(pygame.sprite.Sprite):
     # construtor da classe do jogador
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         w, h = pygame.display.get_surface().get_size()
         self.pos = Vector2(w // 2, h // 2)
         self.vida = 10
-        self.vel = w//40
+        self.vel = w // 40
         self.ponto = 0
-        self.block = pygame.Rect(self.pos.x, self.pos.y, 20, 20)
         self.morto = False
         self.coleta_vida = False
         self.coleta_vel = False
         self.coleta_ponto = False
+        self.image = pygame.image.load('sprites/zezinho_parado.png').convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = w // 2
+        self.rect.y = h // 2
 
     # metodo de movimentacao da classe jogador
     def movimentacao(self, tecla):
         if tecla[pygame.K_UP] and self.pos.y > 0:
-            self.pos.y -= self.vel
+            self.rect.y -= self.vel
 
         if tecla[pygame.K_RIGHT] and self.pos.x < 800:
-            self.pos.x += self.vel
+            self.rect.x += self.vel
 
         if tecla[pygame.K_DOWN] and self.pos.y < 450:
-            self.pos.y += self.vel
+            self.rect.y += self.vel
 
         if tecla[pygame.K_LEFT] and self.pos.x > 0:
-            self.pos.x -= self.vel
-    
+            self.rect.x -= self.vel
+
     # metodo de animacao
     def draw_jogador(self):
         x_pos = self.pos.x
         y_pos = self.pos.y
 
-        self.block = pygame.Rect(x_pos, y_pos, 20, 20)
-        pygame.draw.rect(pygame.display.get_surface(), (183, 111, 122), self.block)
+        self.rect = pygame.Rect(x_pos, y_pos, 20, 20)
+        pygame.draw.rect(pygame.display.get_surface(), (183, 111, 122), self.rect)
 
     # metodo de colisao dos outros items, o parametro recebe uma lista de items
     # e verifica se colidiu com os itens ou inimigos. prevejo que depois teirei de
@@ -44,7 +49,7 @@ class Jogador:
     def colisao_inimigos(self, enemy):
         lista_colisao = []
         for inimigo in enemy:
-            if self.block.colliderect(inimigo):
+            if self.rect.colliderect(inimigo):
                 lista_colisao.append(True)
             else:
                 lista_colisao.append(False)
@@ -52,19 +57,22 @@ class Jogador:
             self.vida -= 1
 
     def colisao_item_vel(self, item):
-        if self.block.colliderect(item):
-            return True
+        if self.rect.colliderect(item):
+            self.coleta_vel = True
+        else:
+            self.coleta_vel = False
 
     def colisao_item_vida(self, item):
-        if self.block.colliderect(item):
+        if self.rect.colliderect(item):
             self.vida += 1
             self.coleta_vida = True
-        
+        else:
+            self.coleta_vida = False
 
     def colisao_item_ponto(self, item):
         lista_colisao = []
         for ponto in item:
-            if self.block.colliderect(ponto):
+            if self.rect.colliderect(ponto):
                 lista_colisao.append(True)
             else:
                 lista_colisao.append(False)
