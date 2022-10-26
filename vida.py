d11e1f934ac5095dee64f5a0bef06f4850001d69
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-10-26 10:47:50 -0300

    Integração com as texturas de vida e velocidade

--- vida.py
+++ vida.py
@@ -1,52 +1,34 @@
 from pygame.math import Vector2
+from pygame.transform import scale
 import pygame
 import random
 
 
-class Vida:
+class Vida(pygame.sprite.Sprite):
     def __init__(self):
-        # Inicia o objeto com um contador para controlar o intervalo de spawn
-        # O objeto também possui uma variável para checar se o objeto já está spawnado 
+        pygame.sprite.Sprite.__init__(self)
         self.tela = pygame.display.get_surface()
         self.largura_tela, self.altura_tela = pygame.display.get_surface().get_size()
         self.tempo_spawn = 0
         self.existe_sprite = False
         self.pos = Vector2(random.randint(0, self.largura_tela - 12), random.randint(0, self.altura_tela - 12))
-        self.block = pygame.Rect(self.pos.x, self.pos.y, 12, 12)
+        self.image = pygame.image.load('sprites/sprite_milho.png')
+        self.image = scale(self.image, (50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = 2000
+        self.rect.y = 2000
 
     def spawn_vida(self, status):
-        if self.existe_sprite:
-            # Caso já exista um objeto spawnado, o objeto tem seu contador zerado
-            self.tempo_spawn = 0
-        elif not self.existe_sprite and self.tempo_spawn < 600:
-            # Caso não exista um objeto spawnado, o contador aumentará até o tempo definido
-            self.tempo_spawn += 1
-        elif not self.existe_sprite and self.tempo_spawn >= 600:
-            # Supondo que o jogo rode a 30fps, o contador irá até 600, ou seja, 20 segundos
-            # Então o objeto é desenhado aleatoriamente na tela (por enquanto sem sprite)
-            # E a variável recebe True até que haja a colisão do jogador com o objeto e variável volte a ser False
-            # Matheus: Nao sei fazer spawnar uma segunda vez tem algum erro na minha logica
-            status = False
-            self.pos = Vector2(random.randint(0, self.largura_tela-12), random.randint(0, self.altura_tela-12))
-            self.block = pygame.Rect(self.pos.x, self.pos.y, 12, 12)
-            self.existe_sprite = True
-            
-
-    # Se o jogador coletar coloca a caixa pra fora da tela pra nao ser coletada depois que respawnar
-    # sera alocado na tela novamente 
-    def coletado(self, status):
         if status:
             self.existe_sprite = False
-            self.pos.x = 2000
-            self.pos.y = 2000
-            self.block = pygame.Rect(self.pos.x, self.pos.y, 12, 12)
-
-    # So desenha na tela se o sprite existir na tela
-    def draw_vida(self):
         if self.existe_sprite:
-            pygame.draw.rect(self.tela, (250, 5, 180), self.block)
-
-    # funcao que deve se repetir em outros objetos
-    def update_vida(self, status):
-        self.spawn_vida(status)
-        self.coletado(status)
+            self.tempo_spawn = 0
+        elif not self.existe_sprite and self.tempo_spawn < 600:
+            self.tempo_spawn += 1
+            self.rect.x = 2000
+            self.rect.y = 2000
+        elif not self.existe_sprite and self.tempo_spawn >= 600:
+            self.pos = Vector2(random.randint(0, self.largura_tela-50), random.randint(0, self.altura_tela-50))
+            self.rect.x = self.pos.x
+            self.rect.y = self.pos.y
+            self.existe_sprite = True
