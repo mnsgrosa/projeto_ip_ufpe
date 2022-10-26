4bab55bbafdc9b3dfa03d42bfc04c9720b9a561c
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-10-26 10:45:30 -0300

    Integração com as texturas de vida e velocidade

--- playground.py
+++ playground.py
@@ -5,25 +5,30 @@ from vida import Vida
 from velocidade import Velocidade
 import numpy as np
 
+
 # Classe que ira conter todos os objetos
 class Main:
     def __init__(self):
+        self.tela = pygame.display.get_surface()
         self.jogador = Jogador()
         self.item_vida = Vida()
         self.item_vel = Velocidade()
         self.next_move = pygame.time.get_ticks() + 100
+        self.sprites = pygame.sprite.Group()
+        self.sprites.add(self.jogador, self.item_vida, self.item_vel)
 
     def update(self, inimigos, items_vel, items_vida, items_ponto, tecla):
-        if pygame.time.get_ticks() >= self.next_move: 
+        if pygame.time.get_ticks() >= self.next_move:
             self.next_move = pygame.time.get_ticks() + 100
             self.jogador.update_jogador(inimigos, items_vel, items_vida, items_ponto, tecla)
-        self.item_vida.update_vida(self.jogador.coleta_vida)
-        self.item_vel.update_velocidade(self.jogador.coleta_vel)
+        self.item_vida.spawn_vida(self.jogador.coleta_vida)
+        self.item_vel.spawn_velocidade(self.jogador.coleta_vel)
+        self.sprites = pygame.sprite.Group()
+        self.sprites.add(self.jogador, self.item_vida, self.item_vel)
 
     def draw_elementos(self):
-        self.jogador.draw_jogador()
-        self.item_vida.draw_vida()
-        self.item_vel.draw_velocidade()
+        self.sprites.draw(self.tela)
+
 
 if __name__ == '__main__':
     unidade = 20
@@ -31,12 +36,12 @@ if __name__ == '__main__':
     SCREEN_HEIGHT = 22.5
 
     clock = pygame.time.Clock()
-    clock.tick(60)
+    clock.tick(30)
 
     bg = pygame.image.load("sprites/background.png")
+    bg = pygame.transform.scale(bg, (unidade * SCREEN_WIDTH, unidade * SCREEN_HEIGHT))
 
     inimigo = pygame.Rect(10, 10, 20, 20)
-    item_vel = pygame.Rect(40, 10, 20, 20)
     item_ponto = pygame.Rect(100, 10, 20, 20)
 
     screen = pygame.display.set_mode((unidade * SCREEN_WIDTH, unidade * SCREEN_HEIGHT))
@@ -48,29 +53,28 @@ if __name__ == '__main__':
 
     jogo = Main()
 
-
     while running:
         antiga_pontuacao = jogo.jogador.ponto
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-        print(jogo.item_vida.pos)
+
+        print((jogo.item_vel.rect.x, jogo.item_vel.rect.y))
 
         if jogo.jogador.morto:
-                running = False
+            running = False
 
-        tecla = pygame.key.get_pressed()        
-        jogo.update([inimigo], jogo.item_vel.block, jogo.item_vida.block, [item_ponto], tecla)
-        
+        tecla = pygame.key.get_pressed()
+        jogo.update([inimigo], jogo.item_vel.rect, jogo.item_vida.rect, [item_ponto], tecla)
+
         if jogo.jogador.ponto != antiga_pontuacao:
-            item_ponto = pygame.Rect(np.random.randint(0, (unidade * SCREEN_WIDTH) - unidade), np.random.randint(0, (unidade * SCREEN_HEIGHT) - 20), 20, 20)
+            item_ponto = pygame.Rect(np.random.randint(0, (unidade * SCREEN_WIDTH) - unidade),
+                                     np.random.randint(0, (unidade * SCREEN_HEIGHT) - 20), 20, 20)
 
         pygame.display.update()
         screen.fill((255, 255, 255))
         screen.blit(bg, (0, 0))
         jogo.draw_elementos()
-        pygame.draw.rect(pygame.display.get_surface(), (255, 0, 0), item_vel)
         pygame.draw.rect(pygame.display.get_surface(), (0, 0, 255), item_ponto)
         pygame.draw.rect(pygame.display.get_surface(), (0, 123, 122), inimigo)
         pygame.display.flip()
