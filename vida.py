7179765e7b0180aa9686a0b940ffd6e82a61164a
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-10-29 23:41:34 -0300

    Adição da barra de vida

--- vida.py
+++ vida.py
@@ -1,34 +1,59 @@
-from pygame.math import Vector2
-from pygame.transform import scale
 import pygame
-import random
+from pygame.transform import scale
+from random import randint
 
 
 class Vida(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.tela = pygame.display.get_surface()
         self.largura_tela, self.altura_tela = pygame.display.get_surface().get_size()
         self.tempo_spawn = 0
         self.existe_sprite = False
-        self.pos = Vector2(random.randint(0, self.largura_tela - 12), random.randint(0, self.altura_tela - 12))
         self.image = pygame.image.load('sprites/sprite_milho.png')
         self.image = scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.x = 2000
-        self.rect.y = 2000
+        self.rect.x, self.rect.y = 2000, 2000
 
-    def spawn_vida(self, status):
-        if status:
+    def spawn_vida(self, coleta_vida):
+        if coleta_vida:
+            self.rect.x, self.rect.y = 2000, 2000
             self.existe_sprite = False
         if self.existe_sprite:
             self.tempo_spawn = 0
         elif not self.existe_sprite and self.tempo_spawn < 390:
             self.tempo_spawn += 1
-            self.rect.x = 2000
-            self.rect.y = 2000
         elif not self.existe_sprite and self.tempo_spawn >= 390:
-            self.pos = Vector2(random.randint(0, self.largura_tela-50), random.randint(0, self.altura_tela-50))
-            self.rect.x = self.pos.x
-            self.rect.y = self.pos.y
+            self.rect.x, self.rect.y = randint(0, self.largura_tela - 50), randint(0, self.altura_tela - 50)
             self.existe_sprite = True
+
+
+class Barra_vida(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.coracao_0 = pygame.image.load('sprites/coracao_0.png')
+        self.coracao_1 = pygame.image.load('sprites/coracao_1.png')
+        self.coracao_2 = pygame.image.load('sprites/coracao_2.png')
+        self.coracao_3 = pygame.image.load('sprites/coracao_3.png')
+        self.coracao_0, self.coracao_1 = scale(self.coracao_0, (150, 50)), scale(self.coracao_1, (150, 50))
+        self.coracao_2, self.coracao_3 = scale(self.coracao_2, (150, 50)), scale(self.coracao_3, (150, 50))
+        self.image = self.coracao_3
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = 20, 10
+
+    def update_barra(self, vida):
+        if vida == 3:
+            self.image = self.coracao_3
+            self.rect = self.image.get_rect()
+            self.rect.x, self.rect.y = 20, 10
+        elif vida == 2:
+            self.image = self.coracao_2
+            self.rect = self.image.get_rect()
+            self.rect.x, self.rect.y = 20, 10
+        elif vida == 1:
+            self.image = self.coracao_1
+            self.rect = self.image.get_rect()
+            self.rect.x, self.rect.y = 20, 10
+        elif vida == 0:
+            self.image = self.coracao_0
+            self.rect = self.image.get_rect()
+            self.rect.x, self.rect.y = 20, 10
