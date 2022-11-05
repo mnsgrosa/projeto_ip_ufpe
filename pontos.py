d29b4acf3b290e77f14d54a3c188d4c33baab660
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-11-04 23:24:21 -0300

    Adicionado som

--- pontos.py
+++ pontos.py
@@ -8,12 +8,14 @@ class Pontos(pygame.sprite.Sprite):
     def __init__(self):
         # Para teste até que se tenha a classe inimigo
         pygame.sprite.Sprite.__init__(self)
+        pygame.mixer.init()
         self.item_droppado = False
         self.tempo_spawn = 0
         self.tela = pygame.display.get_surface()
         self.largura_tela, self.altura_tela = pygame.display.get_surface().get_size()
         self.contador = 0
         self.indice = -1
+        self.som = pygame.mixer.Sound('som_moeda.wav')
         self.coin_0, self.coin_1 = pygame.image.load('sprites/coin_0.png'), pygame.image.load('sprites/coin_1.png')
         self.coin_2, self.coin_3 = pygame.image.load('sprites/coin_2.png'), pygame.image.load('sprites/coin_3.png')
         self.coin_4, self.coin_5 = pygame.image.load('sprites/coin_4.png'), pygame.image.load('sprites/coin_5.png')
@@ -23,19 +25,18 @@ class Pontos(pygame.sprite.Sprite):
         self.sprites = (self.coin_0, self.coin_1, self.coin_2, self.coin_3, self.coin_4, self.coin_5)
         self.image = self.sprites[0]
         self.rect = self.image.get_rect(topleft=(2000, 0))
-        self.pos = Vector2(randint(0, self.largura_tela - 35), randint(0, self.altura_tela - 35))
 
     def spawn_ponto(self, coleta_ponto):
         if coleta_ponto:
             self.item_droppado = False
             self.rect.x, self.rect.y = 2000, 0
+            self.som.play()
         if self.item_droppado:
             self.tempo_spawn = 0
         elif not self.item_droppado and self.tempo_spawn < 30:
             self.tempo_spawn += 1
         elif not self.item_droppado and self.tempo_spawn >= 30:
-            self.pos = Vector2(randint(0, self.largura_tela - 35), randint(0, self.altura_tela - 35))
-            self.rect.x, self.rect.y = self.pos.x, self.pos.y
+            self.rect.x, self.rect.y = randint(0, self.largura_tela - 35), randint(0, self.altura_tela - 35)
             self.item_droppado = True
 
     def animacao(self):
@@ -47,9 +48,7 @@ class Pontos(pygame.sprite.Sprite):
                 if self.indice > 5:
                     self.indice = 0
                 self.image = self.sprites[self.indice]
-                self.rect = self.image.get_rect(topleft=(self.pos.x, self.pos.y))
 
     def update_ponto(self, coleta_ponto):
         self.spawn_ponto(coleta_ponto)
         self.animacao()
-
