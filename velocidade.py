32a93c96a14a653d95f13f912ac6ef33899ef6f1
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-11-04 23:22:50 -0300

    Update velocidade.py

--- velocidade.py
+++ velocidade.py
@@ -7,35 +7,35 @@ import random
 class Velocidade(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        pygame.mixer.init()
         self.tela = pygame.display.get_surface()
         self.largura_tela, self.altura_tela = pygame.display.get_surface().get_size()
         self.tempo_spawn = 0
-        self.existe_sprite = False
-        self.pos = Vector2(random.randint(0, self.largura_tela - 50), random.randint(0, self.altura_tela - 50))
-        self.image = pygame.image.load('sprites/sprite_cafe.png')
-        self.image = scale(self.image, (50, 50))
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 2000
-        self.vel = self.largura_tela // 40
         self.duracao_boost = 0
+        self.existe_sprite = False
         self.boost_ativo = False
+        self.som = pygame.mixer.Sound('sons/som_velocidade.wav')
+        self.som.set_volume(0.09)
+        self.image = scale(pygame.image.load('sprites/sprite_cafe.png'), (50, 50))
+        self.rect = self.image.get_rect(topleft=(0, 2000))
+        self.vel = self.largura_tela // 40
 
-    def spawn_velocidade(self, status):
-        if status:
-            self.rect.x = 0
-            self.rect.y = 2000
+    def spawn_velocidade(self, coleta):
+        if coleta:
+            self.rect.x, self.rect.y = 0, 2000
             self.existe_sprite = False
-            self.boost_ativo = True
         if self.existe_sprite:
             self.tempo_spawn = 0
         elif not self.existe_sprite and not self.boost_ativo and self.tempo_spawn < 210:
             self.tempo_spawn += 1
         elif not self.existe_sprite and not self.boost_ativo and self.tempo_spawn >= 210:
-            self.pos = Vector2(random.randint(0, self.largura_tela - 50), random.randint(0, self.altura_tela - 50))
-            self.rect.x = self.pos.x
-            self.rect.y = self.pos.y
+            self.rect.x, self.rect.y = random.randint(0, self.largura_tela - 50), random.randint(0, self.altura_tela - 50)
             self.existe_sprite = True
+
+    def boost(self, coleta):
+        if coleta:
+            self.som.play()
+            self.boost_ativo = True
         if self.boost_ativo and self.duracao_boost < 180:
             self.duracao_boost += 1
             self.vel = self.largura_tela // 20
@@ -43,3 +43,7 @@ class Velocidade(pygame.sprite.Sprite):
             self.duracao_boost = 0
             self.boost_ativo = False
             self.vel = self.largura_tela // 40
+
+    def update_cafe(self, coleta):
+        self.spawn_velocidade(coleta)
+        self.boost(coleta)
