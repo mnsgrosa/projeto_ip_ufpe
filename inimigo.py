63457f922e0a256f5f89298d739d8d675ab00097
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-11-06 17:05:27 -0300

    Sprite implementado, ñ está rodando

--- inimigo.py
+++ inimigo.py
@@ -1,34 +1,52 @@
-from pygame import Vector2
-from random import randint
 import pygame, sys
 import math
+from pygame import Vector2
+from pygame.transform import scale
+from random import randint
 
 
-# INIMIGO
-
-class Inimigo:
+class Inimigo():
     def __init__(self, jogador):
         self.w, self.h = pygame.display.get_surface().get_size()
-        
-        x = randint(0, w)
-        y = randint(0, h)
+        x, y = randint(0, w - 40), randint(0, h - 60)
 
         while x != jogador.rect.x and y != jogador.rect.y:
-            x = randint(0, w)
-            y = randint(0, h)
+            x, y = randint(0, w - 40), randint(0, h - 60)
 
-        self.pos = Vector2(x, y)
         self.morto = False
         self.angle = math.atan2(self.pos.y - jogador.rect.y, self.pos.x - jogador.rect.x)
         self.dx = math.cos(angle) * self.w // 40
         self.dy = math.sin(angle) * self.w // 40
-        self.image = pygame.Surface((45, 75))
-        self.image.fill((255, 0, 0))
-        self.rect = self.image.get_rect()
-        self.rect.x = self.pos.x
-        self.rect.y = self.pos.y
+        self.spritesheet = scale(pygame.image.load('sprites/sprite_inimigo.png'), (240, 240))
+        self.contador = -0.5
+        self.sprites_esquerda = [self.spritesheet.subsurface((0, 0), (40, 60)),
+                                 self.spritesheet.subsurface((40, 0), (40, 60)),
+                                 self.spritesheet.subsurface((80, 0), (40, 60)),
+                                 self.spritesheet.subsurface((120, 0), (40, 60)),
+                                 self.spritesheet.subsurface((160, 0), (40, 60)),
+                                 self.spritesheet.subsurface((200, 0), (40, 60))]
+        self.sprites_direita = [self.spritesheet.subsurface((0, 60), (40, 60)),
+                                self.spritesheet.subsurface((40, 60), (40, 60)),
+                                self.spritesheet.subsurface((80, 60), (40, 60)),
+                                self.spritesheet.subsurface((120, 60), (40, 60)),
+                                self.spritesheet.subsurface((160, 60), (40, 60)),
+                                self.spritesheet.subsurface((200, 60), (40, 60)),]
+        self.sprites_baixo = [self.spritesheet.subsurface((0, 120), (40, 60)),
+                              self.spritesheet.subsurface((40, 120), (40, 60)),
+                              self.spritesheet.subsurface((80, 120), (40, 60)),
+                              self.spritesheet.subsurface((120, 120), (40, 60)),
+                              self.spritesheet.subsurface((160, 120), (40, 60)),
+                              self.spritesheet.subsurface((200, 120), (40, 60))]
+        self.sprites_cima = [self.spritesheet.subsurface((0, 180), (40, 60)),
+                             self.spritesheet.subsurface((40, 180), (40, 60)),
+                             self.spritesheet.subsurface((80, 180), (40, 60)),
+                             self.spritesheet.subsurface((120, 180), (40, 60)),
+                             self.spritesheet.subsurface((160, 180), (40, 60)),
+                             self.spritesheet.subsurface((200, 180), (40, 60))]
+        self.image = self.sprites_baixo[0]
+        self.rect = self.image.get_rect(topleft=(x, y))
 
-    def update(self, jogador, bala):
+    def update_inimigo(self, jogador, bala):
         self.angle = math.atan2(self.rect.y - jogador.rect.y, self.rect.x - jogador.rect.x)
         self.cos = math.cos(self.angle)
         self.sin = math.sin(self.angle)
@@ -43,25 +61,30 @@ class Inimigo:
         else:
             self.morto = False
 
-        if ((self.cos > 0 and self.cos < 1) and (self.sin < 1 and self.sin > 0)) or ((self.cos > 0 and self.cos < 1) and (self.sin > -1 and self.sin < 0)):
-            return 'direita', self.morto, self.posicao_morto
-        
+        if ((self.cos > 0 and self.cos < 1) and (self.sin < 1 and self.sin > 0)) or (
+                (self.cos > 0 and self.cos < 1) and (self.sin > -1 and self.sin < 0)):
+            return 'direita'
+
         if self.cos == 0 and self.sin == 1:
-            return 'cima', self.morto, self.posicao_morto
-        
-        if ((self.cos < 0 and self.cos > -1) and (self.sin < 1 and self.sin > 0)) or ((self.cos > -1 and self.cos < 0) and (self.sin > -1 and self.sin < 0)):
-            return 'esquerda', self.morto, self.posicao_morto
+            return 'cima'
+
+        if ((self.cos < 0 and self.cos > -1) and (self.sin < 1 and self.sin > 0)) or (
+                (self.cos > -1 and self.cos < 0) and (self.sin > -1 and self.sin < 0)):
+            return 'esquerda'
 
         if self.cos == 0 and self.sin == -1:
-            return 'baixo', self.morto, self.posicao_morto
+            return 'baixo'
 
     def draw(self, jogador, bala):
-        direcao = self.update(jogador, bala)
+        self.contador += 0.5
+        if self.contador == 6:
+            self.contador = 0
+        direcao = self.update_inimigo(jogador, bala)
         if direcao == 'direita':
-            pass
+            self.image = self.sprites_direita[int(self.contador)]
         elif direcao == 'cima':
-            pass
+            self.image = self.sprites_cima[int(self.contador)]
         elif direcao == 'esquerda':
-            pass
+            self.image = self.sprites_esquerda[int(self.contador)]
         elif direcao == 'baixo':
-            pass
+            self.image = self.sprites_baixo[int(self.contador)]
