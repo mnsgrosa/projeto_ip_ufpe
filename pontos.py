a6f11d8b10f7228c5936ab5815c2f134b9915d8d
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-11-06 12:08:19 -0300

    Modificação do funcionamento do spawn de pontos
    obs: as sprites ficaram bugadas ñ sei pq

--- pontos.py
+++ pontos.py
@@ -4,51 +4,47 @@ from pygame.transform import scale
 from random import randint
 
 
-class Pontos(pygame.sprite.Sprite):
+class Pontos():
     def __init__(self):
-        # Para teste até que se tenha a classe inimigo
-        pygame.sprite.Sprite.__init__(self)
         pygame.mixer.init()
-        self.item_droppado = False
         self.tempo_spawn = 0
         self.tela = pygame.display.get_surface()
         self.largura_tela, self.altura_tela = pygame.display.get_surface().get_size()
-        self.contador = 0
-        self.indice = -1
-        self.som = pygame.mixer.Sound('som_moeda.wav')
+        self.contador = 1 / 4
+        self.som = pygame.mixer.Sound('sons/som_moeda.wav')
         self.coin_0, self.coin_1 = pygame.image.load('sprites/coin_0.png'), pygame.image.load('sprites/coin_1.png')
         self.coin_2, self.coin_3 = pygame.image.load('sprites/coin_2.png'), pygame.image.load('sprites/coin_3.png')
         self.coin_4, self.coin_5 = pygame.image.load('sprites/coin_4.png'), pygame.image.load('sprites/coin_5.png')
         self.coin_0, self.coin_1 = scale(self.coin_0, (35, 35)), scale(self.coin_1, (35, 35))
         self.coin_2, self.coin_3 = scale(self.coin_2, (35, 35)), scale(self.coin_3, (35, 35))
         self.coin_4, self.coin_5 = scale(self.coin_4, (35, 35)), scale(self.coin_5, (35, 35))
-        self.sprites = (self.coin_0, self.coin_1, self.coin_2, self.coin_3, self.coin_4, self.coin_5)
+        self.lista_ponto = []
+        self.sprites = [self.coin_0, self.coin_1, self.coin_2, self.coin_3, self.coin_4, self.coin_5]
         self.image = self.sprites[0]
-        self.rect = self.image.get_rect(topleft=(2000, 0))
 
-    def spawn_ponto(self, coleta_ponto):
-        if coleta_ponto:
-            self.item_droppado = False
-            self.rect.x, self.rect.y = 2000, 0
-            self.som.play()
-        if self.item_droppado:
-            self.tempo_spawn = 0
-        elif not self.item_droppado and self.tempo_spawn < 30:
-            self.tempo_spawn += 1
-        elif not self.item_droppado and self.tempo_spawn >= 30:
-            self.rect.x, self.rect.y = randint(0, self.largura_tela - 35), randint(0, self.altura_tela - 35)
-            self.item_droppado = True
+    #morte_inimigo é uma variável booleana
+    def spawn_ponto(self, morte_inimigo, pos_inimigo_morto):
+        if morte_inimigo:
+            self.lista_ponto.append(pos_inimigo_morto)
+        for ponto in self.lista_ponto:
+            self.tela.blit(self.image, ponto)
+
+    #idx_ponto_coletado é o índice do sprite q sofreu a colisão
+    def coleta_ponto(self, ponto_coletado):
+        contador = -1
+        for ponto in self.lista_ponto:
+            contador += 1
+            if ponto == ponto_coletado:
+                self.som.play()
+                del self.lista_ponto[contador]
 
     def animacao(self):
-        if self.item_droppado:
-            self.contador += 1
-            if self.contador >= 3:
-                self.contador = 0
-                self.indice += 1
-                if self.indice > 5:
-                    self.indice = 0
-                self.image = self.sprites[self.indice]
+        if self.contador == 6:
+            self.contador = 0
+        self.image = self.sprites[int(self.contador)]
+        self.contador += 1 / 4
 
-    def update_ponto(self, coleta_ponto):
-        self.spawn_ponto(coleta_ponto)
+    def update_ponto(self, morte_inimigo, pos_inimigo_morto, ponto_coletado):
         self.animacao()
+        self.spawn_ponto(morte_inimigo, pos_inimigo_morto)
+        self.coleta_ponto(ponto_coletado)
