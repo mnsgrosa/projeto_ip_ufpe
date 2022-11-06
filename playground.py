349350fb2645a888d10fc308e918d9699f336386
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-11-06 12:05:13 -0300

    Modificação do funcionamento do spawn de ponto
    obs: as sprites ficaram bugadas ñ sei pq

--- playground.py
+++ playground.py
@@ -20,18 +20,18 @@ class Main:
         self.barra_vida = Barra_vida()
         self.next_move = pygame.time.get_ticks() + 100
         self.sprites = pygame.sprite.Group()
-        self.sprites.add(self.jogador, self.item_vida, self.item_vel, self.item_ponto, self.barra_vida)
+        self.sprites.add(self.jogador, self.item_vida, self.item_vel, self.barra_vida)
 
-    def update(self, inimigos, items_vel, items_vida, items_ponto, tecla):
+    def update(self, inimigos, items_vel, items_vida, items_ponto, tecla, morte_inimigo, pos_inimigo_morto):
+        self.item_ponto.update_ponto(morte_inimigo, pos_inimigo_morto, self.jogador.ponto_coletado)
         if pygame.time.get_ticks() >= self.next_move:
             self.next_move = pygame.time.get_ticks() + 100
             self.jogador.update_jogador(inimigos, items_vel, items_vida, items_ponto, tecla)
         self.item_vida.spawn_vida(self.jogador.coleta_vida)
         self.item_vel.update_cafe(self.jogador.coleta_vel)
-        self.item_ponto.update_ponto(self.jogador.coleta_ponto)
         self.barra_vida.update_barra(self.jogador.vida)
         self.sprites = pygame.sprite.Group()
-        self.sprites.add(self.jogador, self.item_vida, self.item_vel, self.item_ponto, self.barra_vida)
+        self.sprites.add(self.jogador, self.item_vida, self.item_vel, self.barra_vida)
 
     def draw_elementos(self):
         self.sprites.draw(self.tela)
@@ -58,6 +58,8 @@ if __name__ == '__main__':
 
     inimigo_pos = Vector2(randint(0, (unidade * SCREEN_WIDTH) - 22), randint(0, (unidade * SCREEN_HEIGHT) - 22))
     inimigo = pygame.Rect(inimigo_pos.x, inimigo_pos.y, 22, 22)
+    morte_inimigo = False
+    pos_inimigo_morto = (inimigo_pos.x, inimigo_pos.y)
 
     screen = pygame.display.set_mode((unidade * SCREEN_WIDTH, unidade * SCREEN_HEIGHT))
     pygame.display.set_caption('CINGAÇO')
@@ -72,10 +74,10 @@ if __name__ == '__main__':
     pygame.font.init()
     pygame.mixer.init()
 
-    som_start = pygame.mixer.Sound('som_start.wav')
-    som_ganha_vida = pygame.mixer.Sound('som_ganha_vida.wav')
-    som_perde_vida = pygame.mixer.Sound('som_perde_vida.wav')
-    som_game_over = pygame.mixer.Sound('som_game_over.wav')
+    som_start = pygame.mixer.Sound('sons/som_start.wav')
+    som_ganha_vida = pygame.mixer.Sound('sons/som_ganha_vida.wav')
+    som_perde_vida = pygame.mixer.Sound('sons/som_perde_vida.wav')
+    som_game_over = pygame.mixer.Sound('sons/som_game_over.wav')
 
     record = 0
     contador = 0
@@ -130,15 +132,21 @@ if __name__ == '__main__':
                 running = False
 
             tecla = pygame.key.get_pressed()
-            jogo.update([inimigo], jogo.item_vel.rect, jogo.item_vida.rect, jogo.item_ponto.rect, tecla)
+            jogo.update([inimigo], jogo.item_vel.rect, jogo.item_vida.rect, jogo.item_ponto.lista_ponto, tecla, morte_inimigo, pos_inimigo_morto)
             jogo.jogador.vel = jogo.item_vel.vel
 
             if jogo.jogador.vida < antiga_vida:
                 som_perde_vida.play()
-                inimigo = pygame.Rect(randint(0, (unidade * SCREEN_WIDTH) - unidade),
-                                      randint(0, (unidade * SCREEN_HEIGHT) - 22), 22, 22)
+                morte_inimigo = True
+                pos_inimigo_morto = (inimigo_pos.x, inimigo_pos.y)
+                inimigo_pos = Vector2(randint(0, (unidade * SCREEN_WIDTH) - 22),
+                                      randint(0, (unidade * SCREEN_HEIGHT) - 22))
+                inimigo = pygame.Rect(inimigo_pos.x, inimigo_pos.y, 22, 22)
             elif jogo.jogador.vida > antiga_vida:
                 som_ganha_vida.play()
+                morte_inimigo = False
+            else:
+                morte_inimigo = False
 
             if contador == 30:
                 grupo_balas.add(jogo.jogador.atira([inimigo]))
