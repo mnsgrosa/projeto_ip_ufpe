cedef177fa122ebb4e94775b5bf0fa765ab01067
Jonatas-jfs8 <114918687+Jonatas-jfs8@users.noreply.github.com>
2022-11-06 23:30:27 -0300

    Update final

--- pontos.py
+++ pontos.py
@@ -4,13 +4,15 @@ from pygame.transform import scale
 from random import randint
 
 
-class Pontos():
+class Pontos(pygame.sprite.Sprite):
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         pygame.mixer.init()
-        self.tempo_spawn = 0
         self.tela = pygame.display.get_surface()
         self.largura_tela, self.altura_tela = pygame.display.get_surface().get_size()
-        self.contador = 1 / 4
+        self.tempo_spawn = 0
+        self.contador = -0.25
+        self.existe_sprite = True
         self.som = pygame.mixer.Sound('sons/som_moeda.wav')
         self.coin_0, self.coin_1 = pygame.image.load('sprites/coin_0.png'), pygame.image.load('sprites/coin_1.png')
         self.coin_2, self.coin_3 = pygame.image.load('sprites/coin_2.png'), pygame.image.load('sprites/coin_3.png')
@@ -18,33 +20,29 @@ class Pontos():
         self.coin_0, self.coin_1 = scale(self.coin_0, (35, 35)), scale(self.coin_1, (35, 35))
         self.coin_2, self.coin_3 = scale(self.coin_2, (35, 35)), scale(self.coin_3, (35, 35))
         self.coin_4, self.coin_5 = scale(self.coin_4, (35, 35)), scale(self.coin_5, (35, 35))
-        self.lista_ponto = []
         self.sprites = [self.coin_0, self.coin_1, self.coin_2, self.coin_3, self.coin_4, self.coin_5]
         self.image = self.sprites[0]
+        self.rect = self.image.get_rect(topleft=(randint(0, self.largura_tela - 35), randint(0, self.altura_tela - 35)))
 
-    #morte_inimigo é uma variável booleana
-    def spawn_ponto(self, morte_inimigo, pos_inimigo_morto):
-        if morte_inimigo:
-            self.lista_ponto.append(pos_inimigo_morto)
-        for ponto in self.lista_ponto:
-            self.tela.blit(self.image, ponto)
-
-    #idx_ponto_coletado é o índice do sprite q sofreu a colisão
-    def coleta_ponto(self, ponto_coletado):
-        contador = -1
-        for ponto in self.lista_ponto:
-            contador += 1
-            if ponto == ponto_coletado:
-                self.som.play()
-                del self.lista_ponto[contador]
+    def spawn_ponto(self, coleta_ponto):
+        if coleta_ponto:
+            self.som.play()
+            self.existe_sprite = False
+            self.rect.x, self.rect.y = 1000, 1000
+        if self.existe_sprite:
+            self.tempo_spawn = 0
+        elif not self.existe_sprite and self.tempo_spawn < 10:
+            self.tempo_spawn += 1
+        elif not  self.existe_sprite and self.tempo_spawn == 10:
+            self.existe_sprite = True
+            self.rect.x, self.rect.y = randint(0, self.largura_tela - 35), randint(0, self.altura_tela - 35)
 
     def animacao(self):
+        self.contador += 0.25
         if self.contador == 6:
             self.contador = 0
         self.image = self.sprites[int(self.contador)]
-        self.contador += 1 / 4
 
-    def update_ponto(self, morte_inimigo, pos_inimigo_morto, ponto_coletado):
+    def update_ponto(self, coleta_ponto):
         self.animacao()
-        self.spawn_ponto(morte_inimigo, pos_inimigo_morto)
-        self.coleta_ponto(ponto_coletado)
+        self.spawn_ponto(coleta_ponto)
